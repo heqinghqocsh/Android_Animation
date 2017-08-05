c4dc2c0d2954a3c3b680a7d0689a4b1d8ee7e369
HeQing <heqinghqocsh@outlook.com>
2017-08-05 15:24:25 +0800

    加入动画之Zooming a View

--- app/src/main/java/com/example/heqing/animation/MainActivity.java
+++ app/src/main/java/com/example/heqing/animation/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import com.example.heqing.animation.cardflip.CardFlipActivity;
 import com.example.heqing.animation.crossfading.CrossfadeActivity;
 import com.example.heqing.animation.viewpager.ScreenSlideActivity;
+import com.example.heqing.animation.zooming.ZoomingActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -40,7 +41,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @OnClick({R.id.viewpager_zoom, R.id.viewpager_depth, R.id.crossfade,R.id.card_flip})
+    @OnClick({R.id.viewpager_zoom, R.id.viewpager_depth
+            , R.id.crossfade,R.id.card_flip,R.id.zoom_view})
     public void onViewClicked(View view) {
         Intent intent = new Intent();
         switch (view.getId()) {
@@ -57,6 +59,9 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.card_flip:
                 intent.setClass(this, CardFlipActivity.class);
+                break;
+            case R.id.zoom_view:
+                intent.setClass(this, ZoomingActivity.class);
                 break;
         }
         startActivity(intent);
---
+++ app/src/main/java/com/example/heqing/animation/zooming/ZoomingActivity.java
@@ -0,0 +1,163 @@
+package com.example.heqing.animation.zooming;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+
+import com.example.heqing.animation.R;
+
+/**
+ * Created by HeQing on 2017/8/5 0005.
+ */
+
+public class ZoomingActivity extends AppCompatActivity {
+
+    private Animator mCurrentAnimator;
+    private int mShortAnimationDuration;
+    private View thumbView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zooming_layout);
+
+        thumbView = findViewById(R.id.thumb_button_1);
+        mShortAnimationDuration = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+        thumbView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zoomImgFromThumb(thumbView, R.mipmap.demo);
+            }
+        });
+
+    }
+
+    private void zoomImgFromThumb(final View thumbView, int imgResId) {
+        if (mCurrentAnimator != null) {
+            mCurrentAnimator.cancel();
+        }
+        final ImageView expandedImageView = (ImageView) findViewById(R.id.expanded_image);
+        expandedImageView.setImageResource(imgResId);
+        final Rect startBounds = new Rect();
+        final Rect finalBounds = new Rect();
+        final Point globalOffset = new Point();
+
+        thumbView.getGlobalVisibleRect(startBounds);
+        findViewById(R.id.container).getGlobalVisibleRect(finalBounds, globalOffset);
+        startBounds.offset(-globalOffset.x, -globalOffset.y);
+        finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+        float startScale;
+        if (finalBounds.width() / finalBounds.height() >
+                startBounds.width() / startBounds.height()) {
+            // Extend start bounds horizontally
+            startScale = startBounds.hashCode() / finalBounds.height();
+            float startWidth = startScale * finalBounds.width();
+            float deltaWidth = (startWidth - startBounds.width()) / 2;
+            startBounds.left -= deltaWidth;
+            startBounds.right += deltaWidth;
+        } else {
+            // Extend start bounds vertically
+            startScale = (float) startBounds.width() / finalBounds.width();
+            float startHeight = startScale * finalBounds.height();
+            float deltaHeight = (startHeight - startBounds.height()) / 2;
+            startBounds.top -= deltaHeight;
+            startBounds.bottom += deltaHeight;
+        }
+
+        thumbView.setAlpha(0f);
+        expandedImageView.setVisibility(View.VISIBLE);
+
+        // Set the pivot point for SCALE_X and SCALE_Y transformations
+        // to the top-left corner of the zoomed-in view (the default
+        // is the center of the view).
+        expandedImageView.setPivotX(0f);
+        expandedImageView.setPivotY(0f);
+
+        // Construct and run the parallel animation of the four translation and
+        // scale properties (X, Y, SCALE_X, and SCALE_Y).
+        AnimatorSet set = new AnimatorSet();
+        set
+                .play(ObjectAnimator.ofFloat(expandedImageView, View.X,
+                        startBounds.left, finalBounds.left))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y,
+                        startBounds.top, finalBounds.top))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X,
+                        startScale, 1f)).with(ObjectAnimator.ofFloat(expandedImageView,
+                View.SCALE_Y, startScale, 1f));
+        set.setDuration(mShortAnimationDuration);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mCurrentAnimator = null;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                mCurrentAnimator = null;
+            }
+        });
+        set.start();
+        mCurrentAnimator = set;
+
+        // Upon clicking the zoomed-in image, it should zoom back down
+        // to the original bounds and show the thumbnail instead of
+        // the expanded image.
+        final float startScaleFinal = startScale;
+        expandedImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCurrentAnimator != null) {
+                    mCurrentAnimator.cancel();
+                }
+
+                // Animate the four positioning/sizing properties in parallel,
+                // back to their original values.
+                AnimatorSet set = new AnimatorSet();
+                set.play(ObjectAnimator
+                        .ofFloat(expandedImageView, View.X, startBounds.left))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.Y, startBounds.top))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_X, startScaleFinal))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_Y, startScaleFinal));
+                set.setDuration(mShortAnimationDuration);
+                set.setInterpolator(new DecelerateInterpolator());
+                set.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        mCurrentAnimator = null;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        mCurrentAnimator = null;
+                    }
+                });
+                set.start();
+                mCurrentAnimator = set;
+            }
+        });
+
+    }
+
+}
